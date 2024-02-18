2aaf6116762c9cf548844f6d14eab1eab76d08cc
+ <+>
2024-02-18 12:16:13 +0300

    ✏️ commented the 1st challenge

--- Challenges/rot13.py
+++ Challenges/rot13.py
@@ -1,9 +1,17 @@
 
 def rot13(message):
+
     result=""
+    # Iterate through the messages
     for char in message:
+        # Check if the character is alphanumerical ie numbers, letters and special symbols so that we keep white spaces and 
         if char.isalpha():
+            #  Ord converts char to ASCII
+            #  Get the character ASCII. In this case it is 97 for a and 65 for A
              charNumber=ord('a') if  char.islower() else ord('A')
+            #  chr converts Ascii to char 
+            #  Consider a letter H and its ascii is 72 
+            #  (72-65+13) %(modulas) 26= 20 + 65= 85 converted to char its U
              result += chr((ord(char) - charNumber + 13) % 26 + charNumber)
         else:
             result+=char
